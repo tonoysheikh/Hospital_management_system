217cdf20343e0e056ef13bbc5f9741d35361b7a1
Salman Johir Tonoy <165488699+tonoysheikh@users.noreply.github.com>
2024-08-09 05:28:29 +0600

    up

--- Hospital management system/assets/js/main.js
+++ Hospital management system/assets/js/main.js
@@ -192,3 +192,22 @@ gallery : {
 }
 });
 });
+
+function search_doctor() {
+  let input = document.getElementById('searchbar').value
+  input = input.toLowerCase();
+  let x = document.getElementsByClassName('doctor');
+
+  for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+      x[i].style.display = "none";
+    }
+    else {
+      x[i].style.display = "list-item";
+      
+    }
+  }
+}
+
+
+
