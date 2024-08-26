5afd181274753f5d61d945ebfb2d573fe8e4d2a8
Salman Johir Tonoy <165488699+tonoysheikh@users.noreply.github.com>
2024-08-26 23:55:41 +0600

    upload

--- hospital/LANS_HOSPITAL/LANS_HOSPITAL_APP/views.py
+++ hospital/LANS_HOSPITAL/LANS_HOSPITAL_APP/views.py
@@ -8,8 +8,48 @@ def say_hello(request):
 def home(request):
     return render(request, "index.html")
 
-def about(request):
-    return render(request ,"about.html")
+def media(request):
+    return render(request ,"media.html")
 
 def test(request):
-    return render(request ,"test.html")
+    return render(request ,"brain.html")
+def test(request):
+    return render(request ,"bone.html")
+def test(request):
+    return render(request ,"gastro.html")
+def test(request):
+    return render(request ,"mother.html")
+def test(request):
+    return render(request ,"nephrologycenter.html")
+def test(request):
+    return render(request ,"anesthesia.html")
+def test(request):
+    return render(request ,"urology.html")
+def test(request):
+    return render(request ,"Psychologist.html")
+def test(request):
+    return render(request ,"ophthalmology.html")
+def test(request):
+    return render(request ,"nephrology.html")
+def test(request):
+    return render(request ,"find_doctor.html")
+def test(request):
+    return render(request ,"blood_bank.html")
+def test(request):
+    return render(request ,"ICU.html")
+def test(request):
+    return render(request ,"emergency_service.html")
+def test(request):
+    return render(request ,"dialysis_cantre.html")
+def test(request):
+    return render(request ,"feedback.html")
+def test(request):
+    return render(request ,"equipment.html")
+def test(request):
+    return render(request ,"room.html")
+def test(request):
+    return render(request ,"about.html")
+def test(request):
+    return render(request ,"room.html")
+def test(request):
+    return render(request ,"contact.html")
