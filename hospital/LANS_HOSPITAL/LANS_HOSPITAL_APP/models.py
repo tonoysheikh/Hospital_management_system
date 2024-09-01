601a121a1abf8830adca904fbc7c9ab0e5897610
Salman Johir Tonoy <165488699+tonoysheikh@users.noreply.github.com>
2024-09-01 17:34:54 +0600

    up

--- hospital/LANS_HOSPITAL/LANS_HOSPITAL_APP/models.py
+++ hospital/LANS_HOSPITAL/LANS_HOSPITAL_APP/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Doctor(models.Model):
+    name = models.CharField(max_length= 200)
+    speciality = models.TextField()
+    degree = models.TextField()
+    
+    
--- hospital/LANS_HOSPITAL/LANS_HOSPITAL_APP/views.py
+++ hospital/LANS_HOSPITAL/LANS_HOSPITAL_APP/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Doctor
 
 # Create your views here.
 def say_hello(request):
@@ -32,74 +33,12 @@ def ophthalmology(request):
 def nephrology(request):
     return render(request ,"nephrology.html")
 def find_doctor(request):
-    doctors = {
-        "doctor1" : {
-            "name" : "Tanni",
-            "speciality" : "Consultant, Dermatology",
-            "degree" : "MBBS, DDV (SINGAPORE)"
-        },
-        "doctor2" : {
-            "name" : "Dr. Rahul",
-            "speciality" : "Medical Anthropology",
-            "degree" : "MD (Doctor of Medicine), PhD (Doctor of Philosophy)"
-        },
-        "doctor3" : {
-            "name" : "Dr. Nadiya",
-            "speciality" : "Neurosurgery",
-            "degree" : "MD (Doctor of Medicine), MPH (Master of Public Health)"
-        },
-       "doctor4" : {
-            "name" : "Dr. Limon",
-            "speciality" : "Virology",
-            "degree" : "MD (Developer of the polio vaccine)"
-        },
-        "doctor5" : {
-            "name" : "Dr. Halima",
-            "speciality" : "Infectious Diseases",
-            "degree" : "MD (Doctor of Medicine)"
-        },
-        "doctor6" : {
-            "name" : "Dr. Krisna",
-            "speciality" : "Pulmonology",
-            "degree" : " MD (Inventor of the stethoscope)"
-        },
-        "doctor7" : {
-            "name" :"Dr. Bidhan",
-            "speciality" : "Pediatric Neurosurgery",
-            "degree" : "MD (Doctor of Medicine)"
-        },
-        "doctor8" : {
-            "name" :"Dr.Susmita",
-            "speciality" : "Medical Anthropology",
-            "degree" : "M.D., Ph.D."
-        },
-        "doctor9" : {
-            "name" :"Dr. Kunal",
-            "speciality" : "Ethology",
-            "degree" : "Ph.D. (Although not a medical doctor, she is a doctor in her field)"
-        },
-        "doctor10" : {
-            "name" :"Dr. Aniruddha",
-            "speciality" : "Infectious Diseases",
-            "degree" : "Doctor of Medicine (MD)"
-        },
-        "doctor11" : {
-            "name" : "Dr. Rupali",
-            "speciality" : "Cardiac Surgery",
-            "degree" : "Doctor of Medicine (MD), PhD in Cardiothoracic Surgery"
-        },
-        "doctor12" : {
-            "name" : "Dr. Kunal Sarkar",
-            "speciality" : "Immunology",
-            "degree" : "MD (Pioneer of the smallpox vaccine)"
-        }
+    doctor = Doctor.objects.all()
+    return render(request ,"find_doctor.html"  , {"doctor":doctor})
 
-    }
-
-    return render(request ,"find_doctor.html"  , doctors)
-
-def profile(request):
-    return render(request ,"doctor_profile.html")
+def profile(request , pk):
+    doctor = Doctor.objects.get(pk=pk)
+    return render(request ,"doctor_profile.html" , {"doctor":doctor})
 def blood_bank(request):
     return render(request ,"blood_bank.html")
 def ICU(request):
